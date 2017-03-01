46596abd96d1a2b1e21f3b1504d37c0d0cb4aa33
Bernd Porr <mail@berndporr.me.uk>
2017-03-01 21:59:44 +0000

    Updated the code to accomodate the changes in AttysComm

--- app/src/main/java/tech/glasgowneuro/attyseeg/AttysEEG.java
+++ app/src/main/java/tech/glasgowneuro/attyseeg/AttysEEG.java
@@ -292,13 +292,7 @@ public class AttysEEG extends AppCompatActivity {
                     Toast.makeText(getApplicationContext(),
                             "Bluetooth connection problem", Toast.LENGTH_SHORT).show();
                     if (attysComm != null) {
-                        attysComm.cancel();
-                    }
-                    try {
-                        if (attysComm != null) {
-                            attysComm.join();
-                        }
-                    } catch (Exception ee) {
+                        attysComm.stop();
                     }
                     progress.dismiss();
                     finish();
@@ -730,12 +724,7 @@ public class AttysEEG extends AppCompatActivity {
         }
 
         if (attysComm != null) {
-            attysComm.cancel();
-            try {
-                attysComm.join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            attysComm.stop();
             attysComm = null;
             if (Log.isLoggable(TAG, Log.DEBUG)) {
                 Log.d(TAG, "Killed AttysComm");
