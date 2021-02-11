8905e9312ed86c46a56028b90d0b0868ef9cd95c
Bernd Porr <mail@berndporr.me.uk>
2021-02-11 13:44:33 +0000

    Got rid of ECG comments

--- sampleEEGs/plot_all_ch.py
+++ sampleEEGs/plot_all_ch.py
@@ -10,7 +10,7 @@ fig = pl.figure(1);
 #
 a = 0;
 b = len(data)-1;
-# xacc
+#
 pl.subplot(811);
 pl.plot(data[a:b,0],data[a:b,2]);
 pl.xlabel('time/sec');
@@ -21,18 +21,16 @@ pl.plot(data[a:b,0],data[a:b,3]);
 pl.xlabel('time/sec');
 pl.ylabel('delta / V');
 #
-# xmag
 pl.subplot(813);
 pl.plot(data[a:b,0],data[a:b,4]);
 pl.xlabel('time/sec');
 pl.ylabel('theta / V');
 #
-# ymag
 pl.subplot(814);
 pl.plot(data[a:b,0],data[a:b,5]);
 pl.xlabel('time/sec');
 pl.ylabel('alpha / V');
-# zmag
+#
 pl.subplot(815);
 pl.plot(data[a:b,0],data[a:b,6]);
 pl.xlabel('time/sec');
